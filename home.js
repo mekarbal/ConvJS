b1de871c670ce40e0fe4e9bee0797c29c1a8920a
mekarbal <me.karbal@gmail.com>
2019-11-19 11:44:40 +0100

    fst commited

---
+++ home.js
@@ -0,0 +1,42 @@
+function xdecimal()
+{
+decimal = document.getElementById("decimal");
+hexadecimal = document.getElementById("hexadecimal");
+binaire = document.getElementById("binaire");
+octal = document.getElementById("octal");
+ 
+hexadecimal.value = (decimal.value-0).toString(16);
+binaire.value = (decimal.value-0).toString(2);
+octal.value=(decimal.value-0).toString(8)
+}
+ 
+function xhexadecimal()
+{
+decimal = document.getElementById("decimal");
+hexadecimal = document.getElementById("hexadecimal");
+binaire = document.getElementById("binaire");
+octal = document.getElementById("octal");
+ 
+decimal.value = parseInt(hexadecimal.value,16);
+binaire.value = (parseInt(hexadecimal.value,16)).toString(2);
+}
+ 
+function xbinaire()
+{
+decimal = document.getElementById("decimal");
+hexadecimal = document.getElementById("hexadecimal");
+binaire = document.getElementById("binaire");
+ octal = document.getElementById("octal");
+
+decimal.value = parseInt(binaire.value,2);
+hexadecimal.value = (parseInt(binaire.value,2)).toString(16);
+}
+function Octal(){
+decimal = document.getElementById("decimal");
+hexadecimal = document.getElementById("hexadecimal");
+binaire = document.getElementById("binaire");
+ octal = document.getElementById("octal");
+
+decimal.value = parseInt(octal.value,8);
+hexadecimal.value = (parseInt(octal.value,8)).toString(8);
+}
